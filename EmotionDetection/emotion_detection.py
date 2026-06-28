5135030cfb52a010d66a62418a92d85d62174d29
Rishikannan Vedagiri“ <Rishikannan.V@zifornd.com>
2026-06-28 21:35:24 +0530

    Initial push

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,52 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyse):
+    """
+    Analyze emotions from the provided text using Watson NLP.
+    """
+
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/"
+        "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
+
+    header = {
+        "grpc-metadata-mm-model-id":
+        "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    myobj = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Handle blank input / invalid request
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    formatted_response = json.loads(response.text)
+
+    emotion_scores = (
+        formatted_response["emotionPredictions"][0]["emotion"]
+    )
+
+    dominant_emotion = max(
+        emotion_scores,
+        key=emotion_scores.get
+    )
+
+    emotion_scores["dominant_emotion"] = dominant_emotion
+
+    return emotion_scores
